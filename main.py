0dfd22f90b41a363f9f88dd9086caa6699be347d
joy <guanlangaoshou_0@163.com>
2020-10-14 23:15:12 +0800

    update

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import xlsxwriter
 
 
 def analyze(data_frame, col):
     return data_frame.groupby([col])[col].count() \
         .reset_index(name='num').sort_values(by='num', ascending=False)
+
+
+def multi(data_frame, col):
+    data = {}
+    for c in col:
+        new_index = c + '_num'
+        data[c] = data_frame.groupby([c])[c].count() \
+            .reset_index(name=new_index).sort_values(by=new_index, ascending=False)
+
+    return data
 
 
 def run():
@@ -19,15 +30,36 @@ def run():
 
     file_data = os.path.abspath('../../Downloads') + '/userprofileAnalyst.csv'
     if not os.path.exists(file_data):
-        exit()
+        exit(0)
 
     df = pd.read_csv(file_data, encoding='unicode_escape',
                      low_memory=False)
 
-    s = pd.Series(np.random.randn(100).cumsum(), index=np.arange(0, 100))
-    s.plot()
-    plt.show()
-    exit()
+    cols = [
+        'Country',
+        'Age',
+        'EmploymentStatus',
+        'MLToolNextYearSelect',
+        'MLMethodNextYearSelect',
+        'LanguageRecommendationSelect',
+        'JobSkillImportanceBigData',
+        'JobSkillImportanceDegree',
+        'JobSkillImportanceEnterpriseTools',
+        'JobSkillImportancePython',
+        'JobSkillImportanceR',
+        'JobSkillImportanceSQL']
+
+    dt = multi(df, cols)
+    output_file = 'output.xlsx'
+    writer = pd.ExcelWriter(output_file)
+    i = 1
+    for (k, v) in dt.items():
+        v.to_excel(writer, sheet_name='Sheet' + str(i), index=False)
+        i += 1
+    writer.save()
+    writer.close()
+    return
+
     # 列名
     # print(df.columns.values)
     # 数据概览
@@ -75,8 +107,6 @@ def run():
     # 年龄平均值
     age_mean = age_period.mean()['Age']
 
-    exit()
-
     # 性别
     gender = df.groupby(['GenderSelect'])['GenderSelect'].count().reset_index(name='num') \
         .sort_values(by='num', ascending=False)
@@ -86,7 +116,7 @@ def run():
     gender['gender_rate'] = gender['num'] / gender.sum()['num']
     print(gender)
 
-    # 职位
+    # 当前职称
     job = df.groupby(['CurrentJobTitleSelect'])['CurrentJobTitleSelect'].count(). \
         reset_index(name='num').sort_values(by='num', ascending=False)
     print(job)
@@ -101,7 +131,22 @@ def run():
         .reset_index(name='num').sort_values(by='num', ascending=False)
     print(platform)
 
-    writer = pd.ExcelWriter
+    # 工作中使用SQL的频率
+    sql_frequent = analyze(df, 'WorkToolsFrequencySQL')
+    print(sql_frequent)
+
+    # SQL重要性
+    sql_important = analyze(df, 'JobSkillImportanceSQL')
+    print(sql_important)
+
+    # 在线学习课时
+    study_time = analyze(df, 'LearningCategoryOnlineCourses')
+    print(study_time)
+
+    # 就业情况
+    employment_status = analyze(df, 'EmploymentStatus')
+
+    #
 
 
 if __name__ == '__main__':
