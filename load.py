626d0b30ac46a0d4255eeec2ac18ae98843afa88
wangjianwen <wang.jianwen@towngas.com.cn>
2020-11-19 18:12:54 +0800

    up

--- load.py
+++ load.py
@@ -1,18 +1,45 @@
-# -*- coding:utf-8 -*-
-import chardet
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import random
 
 if __name__ == '__main__':
-    f = open('sogou')
+    f = open('sogou', 'rb')
     try:
-        while True:
-            rs = f.readline().split('\t')
-            if rs:
-                str = chardet.detect(rs[2].replace('[', '').replace(']', ''))
-                print(chardet.detect(rs[2].decode('gbk').encode('utf8')))
-                break
-            else:
-                break
+        writer1 = open('sogou.origin', 'a')
+        with open('sogou.extend', 'a') as writer:
+            while True:
+                row = f.readline()
+                if row:
+                    rs = row.split('\t')
+                    year = random.randint(2018, 2020)
+                    month = random.randint(1, 12)
+                    day = random.randint(1, 28)
+                    hour = random.randint(1, 23)
+                    rs[0] = '{0}-{1}-{2} {3}'.format(year, month, day, rs[0])
+                    rs[2] = rs[2].decode('gbk', 'ignore').encode('utf-8').replace('[', '').replace(']', '')
+
+                    if len(rs) > 4:
+                        rs[4] = rs[4].replace('\r\n', '').replace('\n', '')
+                        ret = rs[3].split(' ')
+                        if len(ret) == 2:
+                            rs[3] = ret[0]
+                            order = ret[1]
+                        else:
+                            order = ''
+                    else:
+                        rs.append('')
+                        order = ''
+
+                    writer1.write('\t'.join(rs) + '\n')
+
+                    rs.append(str(order))
+                    rs.append(str(year))
+                    rs.append(str(month))
+                    rs.append(str(day))
+                    rs.append(str(hour))
+                    writer.write('\t'.join(rs) + '\n')
+                else:
+                    break
     finally:
         f.close()
-
-    # .encode("utf-8")
+        print('finished')
